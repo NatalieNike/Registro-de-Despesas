c470cbea7cecdcf208a6e6df40d4f29816599969
Natalie Nike <silvanatalie72@gmail.com>
2024-07-31 18:13:32 -0300

    Adicionando gravação de ações no local storage

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+}
+
+class Bd {
+    gravar(d) {
+        localStorage.setItem('despesa', JSON.stringify(d))
+    }
+}
+
+let bd = new Bd()
+
 function cadastrarDespesa() {
     let ano = document.getElementById('ano')
     let mes = document.getElementById('mes')
@@ -5,4 +24,10 @@ function cadastrarDespesa() {
     let tipo = document.getElementById('tipo')
     let descricao = document.getElementById('descricao')
     let valor = document.getElementById('valor')
+
+    let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+
+    bd.gravar(despesa)
 }
+
+
